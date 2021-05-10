2c9f43760183bf0a1244eace3e2b0b06dce8969e
edjroz <eduardo.j.diaz.rodriguez@gmail.com>
2021-05-10 09:47:10 -0400

    extend benchmarks from iajrz hybrid cache storage

--- x/pocketcore/types/cache_test.go
+++ x/pocketcore/types/cache_test.go
@@ -18,7 +18,7 @@ func InitCacheTest() {
 	// init cache in memory
 	InitConfig(&HostedBlockchains{
 		M: make(map[string]HostedBlockchain),
-	}, logger, sdk.DefaultTestingPocketConfig())
+	}, logger, sdk.DefaultTestingPocketConfig(), false)
 }
 
 func TestMain(m *testing.M) {
@@ -80,6 +80,94 @@ func TestAllEvidence_AddGetEvidence(t *testing.T) {
 	}
 	SetProof(header, RelayEvidence, proof, sdk.NewInt(100000))
 	assert.True(t, reflect.DeepEqual(GetProof(header, RelayEvidence, 0), proof))
+}
+func BenchmarkAllEvidence_AddEvidence(b *testing.B) {
+	benchmarks := []struct {
+		name      string
+		customMap bool
+	}{
+		{name: "stdlib sync map", customMap: false},
+		{name: "custom lock", customMap: true},
+	}
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+				ClearEvidence()
+				globalEvidenceSealedMap = NewCustomSyncMap(bm.customMap)
+				appPubKey := getRandomPubKey().RawString()
+				servicerPubKey := getRandomPubKey().RawString()
+				clientPubKey := getRandomPubKey().RawString()
+				ethereum := hex.EncodeToString([]byte{0001})
+				header := SessionHeader{
+					ApplicationPubKey:  appPubKey,
+					Chain:              ethereum,
+					SessionBlockHeight: 1,
+				}
+				proof := RelayProof{
+					Entropy:            0,
+					RequestHash:        header.HashString(), // fake
+					SessionBlockHeight: 1,
+					ServicerPubKey:     servicerPubKey,
+					Blockchain:         ethereum,
+					Token: AAT{
+						Version:              "0.0.1",
+						ApplicationPublicKey: appPubKey,
+						ClientPublicKey:      clientPubKey,
+						ApplicationSignature: "",
+					},
+					Signature: "",
+				}
+				b.StartTimer()
+				SetProof(header, RelayEvidence, proof, sdk.NewInt(100000))
+			}
+		})
+	}
+}
+
+func BenchmarkAllEvidence_GetEvidence(b *testing.B) {
+	benchmarks := []struct {
+		name      string
+		customMap bool
+	}{
+		{name: "stdlib sync map", customMap: false},
+		{name: "custom lock", customMap: true},
+	}
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+				ClearEvidence()
+				globalEvidenceSealedMap = NewCustomSyncMap(bm.customMap)
+				appPubKey := getRandomPubKey().RawString()
+				servicerPubKey := getRandomPubKey().RawString()
+				clientPubKey := getRandomPubKey().RawString()
+				ethereum := hex.EncodeToString([]byte{0001})
+				header := SessionHeader{
+					ApplicationPubKey:  appPubKey,
+					Chain:              ethereum,
+					SessionBlockHeight: 1,
+				}
+				proof := RelayProof{
+					Entropy:            0,
+					RequestHash:        header.HashString(), // fake
+					SessionBlockHeight: 1,
+					ServicerPubKey:     servicerPubKey,
+					Blockchain:         ethereum,
+					Token: AAT{
+						Version:              "0.0.1",
+						ApplicationPublicKey: appPubKey,
+						ClientPublicKey:      clientPubKey,
+						ApplicationSignature: "",
+					},
+					Signature: "",
+				}
+				SetProof(header, RelayEvidence, proof, sdk.NewInt(100000))
+				b.StartTimer()
+				_ = GetProof(header, RelayEvidence, 0)
+			}
+		})
+	}
 }
 
 func TestAllEvidence_DeleteEvidence(t *testing.T) {
@@ -184,6 +272,7 @@ func TestAllEvidence_GetTotalProofs(t *testing.T) {
 }
 
 func TestSetGetSession(t *testing.T) {
+
 	session := NewTestSession(t, hex.EncodeToString(Hash([]byte("foo"))))
 	session2 := NewTestSession(t, hex.EncodeToString(Hash([]byte("bar"))))
 	SetSession(session)
@@ -197,7 +286,44 @@ func TestSetGetSession(t *testing.T) {
 	assert.True(t, found)
 	assert.Equal(t, s, session2)
 }
+func BenchmarkSetSession(b *testing.B) {
+	benchmarks := []struct {
+		name      string
+		customMap bool
+	}{
+		{name: "stdlib sync map", customMap: false},
+		{name: "custom lock", customMap: true},
+	}
 
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			b.StopTimer()
+			session := NewBenchSession(b, hex.EncodeToString(Hash([]byte("foo"))))
+			b.StartTimer()
+			SetSession(session)
+		})
+	}
+}
+
+func BenchmarkGetSession(b *testing.B) {
+	benchmarks := []struct {
+		name      string
+		customMap bool
+	}{
+		{name: "stdlib sync map", customMap: false},
+		{name: "custom lock", customMap: true},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			b.StopTimer()
+			session := NewBenchSession(b, hex.EncodeToString(Hash([]byte("foo"))))
+			SetSession(session)
+			b.StartTimer()
+			_, _ = GetSession(session.SessionHeader)
+		})
+	}
+}
 func TestDeleteSession(t *testing.T) {
 	session := NewTestSession(t, hex.EncodeToString(Hash([]byte("foo"))))
 	SetSession(session)
@@ -220,6 +346,23 @@ func TestClearCache(t *testing.T) {
 }
 
 func NewTestSession(t *testing.T, chain string) Session {
+	appPubKey := getRandomPubKey()
+	var vals []sdk.Address
+	for i := 0; i < 5; i++ {
+		nodePubKey := getRandomPubKey()
+		vals = append(vals, sdk.Address(nodePubKey.Address()))
+	}
+	return Session{
+		SessionHeader: SessionHeader{
+			ApplicationPubKey:  appPubKey.RawString(),
+			Chain:              chain,
+			SessionBlockHeight: 1,
+		},
+		SessionKey:   appPubKey.RawBytes(), // fake
+		SessionNodes: vals,
+	}
+}
+func NewBenchSession(b *testing.B, chain string) Session {
 	appPubKey := getRandomPubKey()
 	var vals []sdk.Address
 	for i := 0; i < 5; i++ {
